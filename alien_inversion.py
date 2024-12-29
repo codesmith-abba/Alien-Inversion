f0b94c69187ecbaa737ff01f2cdef44fb3b12e55
issteams <issteamcompany@gmail.com>
2024-12-29 15:05:50 +0100

    initial commit

---
+++ alien_inversion.py
@@ -0,0 +1,100 @@
+from settings import Settings
+from ship import Ship
+from bullet import Bullet
+import sys
+import pygame
+
+class AlienInversion:
+    """
+    Overral class to manage game assets and behavior
+    """
+    def __init__(self):
+        """
+        Initialize game and create game resources
+        """
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
+        pygame.display.set_caption("Alien Inversion")
+        
+        # Ship
+        self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
+        
+    def run_game(self):
+        """
+        Start the main game loop
+        """
+        while True:
+            # watch for keyboard and mouse events
+            self._check_events()
+            self.ship.update()
+            self.bullets.update()
+            self._update_bullets()
+            # Redraw the screen during each pass through the loop
+            self._update_screen()
+            
+            self.clock.tick(65)
+    
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+                
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+    
+    def _check_keydown_events(self, event):
+        """Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            # Move the ship to the right
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+        elif event.key == pygame.K_q:
+            sys.exit()
+    
+    def _check_keyup_events(self, event):
+        """Respond to key releases."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+    
+    def _update_bullets(self):
+        """Update the position of bullets and remove old bullets"""
+        # Update the position of bullets
+        self.bullets.update()
+        
+        # Get rid of the game that have disappeard
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+    
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+        # Make the most recently drawn screen visible
+        pygame.display.flip()
+    
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+if __name__ == "__main__":
+    # make game instance and run the game
+    ai = AlienInversion()
+    ai.run_game()
